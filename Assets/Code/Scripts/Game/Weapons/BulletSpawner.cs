faffd4a22549e9bc914dd170b84202e96173db0a
zolori <alban38.niemaz@gmail.com>
2024-02-20 16:07:36 +0100

    ajustement direction tirs

--- Assets/Code/Scripts/Game/Weapons/BulletSpawner.cs
+++ Assets/Code/Scripts/Game/Weapons/BulletSpawner.cs
@@ -77,7 +77,7 @@ public class BulletSpawner : MonoBehaviour
             {
                 Vector2 direction = target.position - transform.position;
                 float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                spawnedBullet.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle - 90));
+                spawnedBullet.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
             }
         }
     }
