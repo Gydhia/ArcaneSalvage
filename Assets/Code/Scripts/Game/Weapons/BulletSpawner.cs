d68b5b91ea65fa0d1316eb37f462414b9758d09a
zolori <alban38.niemaz@gmail.com>
2024-02-20 14:11:07 +0100

    projectiles

--- Assets/Code/Scripts/Game/Weapons/BulletSpawner.cs
+++ Assets/Code/Scripts/Game/Weapons/BulletSpawner.cs
@@ -6,49 +6,76 @@ using UnityEngine;
 public class BulletSpawner : MonoBehaviour
 {
     enum SpawnerType { Straight, Spin }
+    enum GameModePhase { Phase1, Phase2 }
 
 
     [Header("Bullet Attributes")]
     public GameObject bullet;
     public float bulletLife = 1f;
     public float speed = 1f;
+    public GameObject target;
 
 
     [Header("Spawner Attributes")]
     [SerializeField] private SpawnerType spawnerType;
+    [SerializeField] private GameModePhase gameModePhase;
     [SerializeField] private float firingRate = 1f;
 
 
     private GameObject spawnedBullet;
     private float timer = 0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
 
-    // Update is called once per frame
+    void Start() { }
+
     void Update()
     {
         timer += Time.deltaTime;
         if (spawnerType == SpawnerType.Spin) transform.eulerAngles = new Vector3(0f, 0f, transform.eulerAngles.z + 1f);
         if (timer >= firingRate)
         {
-            Fire();
+            if (gameModePhase == GameModePhase.Phase1)
+            {
+                FindClosestEnemy();
+                Fire();
+            }
+            else
+            {
+                // code phase 2
+            }
             timer = 0;
         }
     }
 
+    void FindClosestEnemy()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        target = null;
+        float minDistance = Mathf.Infinity;
+        Vector3 playerPosition = transform.position;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distance = Vector3.Distance(playerPosition, enemy.transform.position);
+            if (distance < minDistance)
+            {
+                target = enemy;
+                minDistance = distance;
+            }
+        }
+    }
 
     private void Fire()
     {
         if (bullet)
         {
             spawnedBullet = Instantiate(bullet, transform.position, Quaternion.identity);
-            spawnedBullet.GetComponent<Bullet>().speed = speed;
-            spawnedBullet.GetComponent<Bullet>().bulletLife = bulletLife;
-            spawnedBullet.transform.rotation = transform.rotation;
+            spawnedBullet.GetComponent<Projectile>().speed = speed;
+            spawnedBullet.GetComponent<Projectile>().bulletLife = bulletLife;
+
+            Vector3 direction = target.transform.position - transform.position; // Direction vers la cible
+            Quaternion rotation = Quaternion.LookRotation(direction); // Calcul de la rotation nécessaire
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10); // Applique la rotation avec interpolation
         }
     }
 }
