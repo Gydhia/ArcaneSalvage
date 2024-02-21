e4ef6129cc74b5d35f280fb982ec26c834ff346b
Alexis <75241737+4lexisFR@users.noreply.github.com>
2024-02-22 00:36:48 +0100

    Player animation

--- Assets/Code/Scripts/Game/Player/PlayerAnimationSystem.cs
+++ Assets/Code/Scripts/Game/Player/PlayerAnimationSystem.cs
@@ -6,6 +6,7 @@ using static UnityEngine.EventSystems.EventTrigger;
 using UnityEngine.SocialPlatforms;
 using Unity.Transforms;
 using Assets.Code.Scripts.Game.Player;
+using Unity.Physics;
 
 public partial struct PlayerAnimationSystem : ISystem
 {
@@ -18,14 +19,33 @@ public partial struct PlayerAnimationSystem : ISystem
             return;
         }
 
-        foreach (var (transform, playerComponent, entity) in SystemAPI.Query<LocalTransform, PlayerControlsAuthoring>().WithEntityAccess())
+        entityManager = state.EntityManager;
+        EntityCommandBuffer ECB = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
+
+        foreach (var (transform, playerComponent, velocity, entity) in SystemAPI.Query<LocalTransform, RefRW<InputVariables>, PhysicsVelocity>().WithEntityAccess())
         {
             if (!entityManager.HasComponent<VisualsReferenceComponent>(entity))
             {
+                GameObject playerVisuals = Object.Instantiate(animationVisualPrefabs.Player);
+                ECB.AddComponent(entity, new VisualsReferenceComponent { gameObject = playerVisuals});
+            }
+            else
+            {
+                VisualsReferenceComponent playerVisualsReference = entityManager.GetComponentData<VisualsReferenceComponent>(entity);
 
+                playerVisualsReference.gameObject.transform.position = transform.Position;
+                //playerVisualsReference.gameObject.transform.rotation = transform.Rotation;
+
+                //playerVisualsReference.gameObject.GetComponent<Animator>().SetBool("Idle", !SystemAPI.GetSingleton<InputComponent>().CanMove);
+                PlayerAnimationBehavior animation = playerVisualsReference.gameObject.GetComponent<PlayerAnimationBehavior>();
+                animation.ToggleIdle(!SystemAPI.GetSingleton<InputComponent>().CanMove);
+                animation.UpdateVelocity(velocity.Linear, playerComponent.ValueRO.IsPhaseTwo);
+                
+                
             }
 
         }
-        entityManager = state.EntityManager;
+        ECB.Playback(entityManager);
+        ECB.Dispose();
     }
 }
